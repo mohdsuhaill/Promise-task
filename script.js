fd88adcd60bed135632d96c819e308f3b1708385
mohdsuhaill <suhailabdmohammed@gmail.com>
2024-06-11 16:38:28 +0530

    update

---
+++ script.js
@@ -0,0 +1,12 @@
+const image = document.getElementById("image")
+
+const result = fetch("https://api.thecatapi.com/v1/images/search")
+result
+  .then((data)=> data.json())
+  .then((ele)=>{
+    for(let i=0;i<ele.length;i++){
+        image.src = ele[i].url
+    }
+  })
+  .catch((error)=>console.log(error));
+
