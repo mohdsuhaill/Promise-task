02f475b6527cc1a992988599af099a9370a834bc
mohdsuhaill <suhailabdmohammed@gmail.com>
2024-06-11 16:45:40 +0530

    update

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+// getting the element...
 const image = document.getElementById("image")
 
+// getting api to img data in  fetch ...
 const result = fetch("https://api.thecatapi.com/v1/images/search")
 result
   .then((data)=> data.json())
